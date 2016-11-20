57256cbdb792fa61ecb943fe2fdbb7009b48fbcb
Kevin Klein <KevinHKlein@me.com>
2016-11-20 20:35:03 +0100

    Anlegen der Tabellen-XML-Datei

--- src/XMLExporter.cpp
+++ src/XMLExporter.cpp
@@ -37,323 +37,73 @@ POSSIBILITY OF SUCH DAMAGE.
 
 #include "XMLExporter.h"
 #include "common.h"
-
-#define MY_ENCODING "UTF-8"
-
-xmlChar *ConvertInput(const char *in, const char *encoding);
+#include "DbConnector.h"
 
 void
 XMLExporter::exportToFS()
-{
-  LIBXML_TEST_VERSION
-  
+{  
   int rc;
   xmlTextWriterPtr writer;
   xmlChar *tmp;
-  std::vector<xmlChar> chars;
+  std::vector<xmlChar> encBuffer;
+  
+	for ( DbTableDesc tbl : this->m_tbls  )
+    {
+		
+		writer = xmlNewTextWriterFilename( wstring_tostring( tbl.tblName ).c_str(), 0 );
+		if ( writer == NULL ) {
+			std::cerr << "testXmlwriterFilename: Error creating the xml writer\n";
+			
+			return;
+		}
+		
+		rc = xmlTextWriterStartDocument( writer, NULL, "UTF-8", NULL );
+		if ( rc < 0 ) {
+			std::cerr << "testXmlwriterFilename: Error at xmlTextWriterStartDocument\n";
+			
+			return;
+		}
+		
+		rc = xmlTextWriterStartElement( writer, BAD_CAST "table" );
+		if ( rc < 0 ) {
+			std::cerr << "testXmlwriterFilename: Error at xmlTextWriterStartElement\n";
+			
+			return;
+		}
+		
+		wstring_toxmlChar( tbl.tblName, encBuffer );
+		rc = xmlTextWriterWriteElement( writer, BAD_CAST "tablename", &encBuffer[0] );
+		if ( rc < 0 ) {
+			std::cerr << "testXmlwriterFilename: Error at xmlTextWriterWriteElement\n";
+			
+			return;
+		}
+		
+		wstring_toxmlChar( tbl.tblType, encBuffer );
+		rc = xmlTextWriterWriteElement( writer, BAD_CAST "tabletyp", &encBuffer[0] );
+		if ( rc < 0 ) {
+			std::cerr << "testXmlwriterFilename: Error at xmlTextWriterWriteElement\n";
+			
+			return;
+		}
+		
+		rc = xmlTextWriterEndDocument( writer );
+		if ( rc < 0 ) {
+			std::cerr << "testXmlwriterFilename: Error at xmlTextWriterEndDocument\n";
+			
+			return;
+		}
 
-    /* Create a new XmlWriter for uri, with no compression. */
-    writer = xmlNewTextWriterFilename("test.xml", 0);
-    if (writer == NULL) {
-        printf("testXmlwriterFilename: Error creating the xml writer\n");
-        return;
+		xmlFreeTextWriter(writer);
+
+		for ( DbColDesc cl : tbl.tblCols  )
+		{
+			std::wcout << cl.colName
+			<< L" - " << cl.colType
+			<< L'(' <<  cl.colLength << L')'
+			<< L" Default: " << cl.colDefaultVal
+			<< L" Nullable: " << cl.colNullable
+			<< std::endl;
+		}	
     }
-
-    /* Start the document with the xml default for the version,
-     * encoding ISO 8859-1 and the default for the standalone
-     * declaration. */
-    rc = xmlTextWriterStartDocument(writer, NULL, MY_ENCODING, NULL);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartDocument\n");
-        return;
-    }
-
-    /* Start an element named "EXAMPLE". Since thist is the first
-     * element, this will be the root element of the document. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "EXAMPLE");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Write a comment as child of EXAMPLE.
-     * Please observe, that the input to the xmlTextWriter functions
-     * HAS to be in UTF-8, even if the output XML is encoded
-     * in iso-8859-1 */
-    wstring_toxmlChar( L"This is a comment with special chars: <äöü>", chars );
-    tmp = ConvertInput("This is a comment with special chars: <äöü>",
-                       MY_ENCODING);
-    rc = xmlTextWriterWriteComment(writer, &chars[0]);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteComment\n");
-        return;
-    }
-    if (tmp != NULL) xmlFree(tmp);
-
-    /* Start an element named "ORDER" as child of EXAMPLE. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "ORDER");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Add an attribute with name "version" and value "1.0" to ORDER. */
-    rc = xmlTextWriterWriteAttribute(writer, BAD_CAST "version",
-                                     BAD_CAST "1.0");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteAttribute\n");
-        return;
-    }
-
-    /* Add an attribute with name "xml:lang" and value "de" to ORDER. */
-    rc = xmlTextWriterWriteAttribute(writer, BAD_CAST "xml:lang",
-                                     BAD_CAST "de");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteAttribute\n");
-        return;
-    }
-
-    /* Write a comment as child of ORDER */
-    tmp = ConvertInput("<äöü>", MY_ENCODING);
-    rc = xmlTextWriterWriteFormatComment(writer,
-		     "This is another comment with special chars: %s",
-		     tmp);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteFormatComment\n");
-        return;
-    }
-    if (tmp != NULL) xmlFree(tmp);
-
-    /* Start an element named "HEADER" as child of ORDER. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "HEADER");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Write an element named "X_ORDER_ID" as child of HEADER. */
-    rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "X_ORDER_ID",
-                                         "%010d", 53535);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteFormatElement\n");
-        return;
-    }
-
-    /* Write an element named "CUSTOMER_ID" as child of HEADER. */
-    rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "CUSTOMER_ID",
-                                         "%d", 1010);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteFormatElement\n");
-        return;
-    }
-
-    /* Write an element named "NAME_1" as child of HEADER. */
-    tmp = ConvertInput("Müller", MY_ENCODING);
-    rc = xmlTextWriterWriteElement(writer, BAD_CAST "NAME_1", tmp);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteElement\n");
-        return;
-    }
-    if (tmp != NULL) xmlFree(tmp);
-
-    /* Write an element named "NAME_2" as child of HEADER. */
-    tmp = ConvertInput("Jörg", MY_ENCODING);
-    rc = xmlTextWriterWriteElement(writer, BAD_CAST "NAME_2", tmp);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteElement\n");
-        return;
-    }
-    if (tmp != NULL) xmlFree(tmp);
-
-    /* Close the element named HEADER. */
-    rc = xmlTextWriterEndElement(writer);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterEndElement\n");
-        return;
-    }
-
-    /* Start an element named "ENTRIES" as child of ORDER. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "ENTRIES");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Start an element named "ENTRY" as child of ENTRIES. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "ENTRY");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Write an element named "ARTICLE" as child of ENTRY. */
-    rc = xmlTextWriterWriteElement(writer, BAD_CAST "ARTICLE",
-                                   BAD_CAST "<Test>");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteElement\n");
-        return;
-    }
-
-    /* Write an element named "ENTRY_NO" as child of ENTRY. */
-    rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "ENTRY_NO", "%d",
-                                         10);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteFormatElement\n");
-        return;
-    }
-
-    /* Close the element named ENTRY. */
-    rc = xmlTextWriterEndElement(writer);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterEndElement\n");
-        return;
-    }
-
-    /* Start an element named "ENTRY" as child of ENTRIES. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "ENTRY");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Write an element named "ARTICLE" as child of ENTRY. */
-    rc = xmlTextWriterWriteElement(writer, BAD_CAST "ARTICLE",
-                                   BAD_CAST "<Test 2>");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteElement\n");
-        return;
-    }
-
-    /* Write an element named "ENTRY_NO" as child of ENTRY. */
-    rc = xmlTextWriterWriteFormatElement(writer, BAD_CAST "ENTRY_NO", "%d",
-                                         20);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteFormatElement\n");
-        return;
-    }
-
-    /* Close the element named ENTRY. */
-    rc = xmlTextWriterEndElement(writer);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterEndElement\n");
-        return;
-    }
-
-    /* Close the element named ENTRIES. */
-    rc = xmlTextWriterEndElement(writer);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterEndElement\n");
-        return;
-    }
-
-    /* Start an element named "FOOTER" as child of ORDER. */
-    rc = xmlTextWriterStartElement(writer, BAD_CAST "FOOTER");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterStartElement\n");
-        return;
-    }
-
-    /* Write an element named "TEXT" as child of FOOTER. */
-    rc = xmlTextWriterWriteElement(writer, BAD_CAST "TEXT",
-                                   BAD_CAST "This is a text.");
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterWriteElement\n");
-        return;
-    }
-
-    /* Close the element named FOOTER. */
-    rc = xmlTextWriterEndElement(writer);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterEndElement\n");
-        return;
-    }
-
-    /* Here we could close the elements ORDER and EXAMPLE using the
-     * function xmlTextWriterEndElement, but since we do not want to
-     * write any other elements, we simply call xmlTextWriterEndDocument,
-     * which will do all the work. */
-    rc = xmlTextWriterEndDocument(writer);
-    if (rc < 0) {
-        printf
-            ("testXmlwriterFilename: Error at xmlTextWriterEndDocument\n");
-        return;
-    }
-
-    xmlFreeTextWriter(writer);  
-}
-
-xmlChar *
-ConvertInput(const char *in, const char *encoding)
-{
-    xmlChar *out;
-    int ret;
-    int size;
-    int out_size;
-    int temp;
-    xmlCharEncodingHandlerPtr handler;
-
-    if (in == 0)
-        return 0;
-
-    handler = xmlFindCharEncodingHandler(encoding);
-
-    if (!handler) {
-        printf("ConvertInput: no encoding handler found for '%s'\n",
-               encoding ? encoding : "");
-        return 0;
-    }
-
-    size = (int) strlen(in) + 1;
-    out_size = size * 2 - 1;
-    out = (unsigned char *) xmlMalloc((size_t) out_size);
-
-    if (out != 0) {
-        temp = size - 1;
-        ret = handler->input(out, &out_size, (const xmlChar *) in, &temp);
-        if ((ret < 0) || (temp - size + 1)) {
-            if (ret < 0) {
-                printf("ConvertInput: conversion wasn't successful.\n");
-            } else {
-                printf
-                    ("ConvertInput: conversion wasn't successful. converted: %i octets.\n",
-                     temp);
-            }
-
-            xmlFree(out);
-            out = 0;
-        } else {
-            out = (unsigned char *) xmlRealloc(out, out_size + 1);
-            out[out_size] = 0;  /*null terminating out */
-        }
-    } else {
-        printf("ConvertInput: no mem\n");
-    }
-
-    return out;
 }
